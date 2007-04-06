3da06f416dee9b6555b497c91079f978e13bf0cb
whgcompt <whgcompt@b89ababa-7a29-0410-b27f-27df3c269e39>
2007-04-06 13:47:22 +0000

    被请求生成的类型必须是接口；顺便值得一提的是，下列类型需要在Helper中引入： 1）被请求的类型 2）参数和返回值的类型 3）参数和返回值Helper的类型

--- portlet/src/portlet/rebind/PortableHelperGenerator.java
+++ portlet/src/portlet/rebind/PortableHelperGenerator.java
@@ -2,10 +2,13 @@ package portlet.rebind;
 
 import java.io.PrintWriter;
 
+import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.ext.Generator;
 import com.google.gwt.core.ext.GeneratorContext;
 import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
+import com.google.gwt.core.ext.typeinfo.JType;
+import com.google.gwt.core.ext.typeinfo.TypeOracle;
 import com.google.gwt.user.rebind.ClassSourceFileComposerFactory;
 import com.google.gwt.user.rebind.SourceWriter;
 
@@ -13,6 +16,14 @@ public class PortableHelperGenerator extends Generator {
 
 	public String generate(TreeLogger logger, GeneratorContext context,
 			String typeName) throws UnableToCompleteException {
+
+		TypeOracle typeOracle = context.getTypeOracle();
+		assert (typeOracle != null);
+
+		JType requestedType = typeOracle.findType(typeName);
+
+		if ( null == requestedType.isInterface() )
+			return typeName;
 
 		// init
 		int nameIndex = typeName.lastIndexOf('.');
@@ -22,30 +33,35 @@ public class PortableHelperGenerator extends Generator {
 
 		// get source writer
 		SourceWriter sw = getSourceWriter(logger, context);
-		if ( null == sw ) {
-			return qualifiedName;
+		if (null == sw) {
+			return getQualifiedName(false);
 		}
-		
+
 		// write
 		sw.print("public String getString() { return \"I love u.\"; }");
 		sw.commit(logger);
 
+		return getQualifiedName(true);
+	}
+
+	private String getQualifiedName(boolean wrote) {
+		System.out.println(qualifiedName + "\twrote: " + wrote);
 		return qualifiedName;
 	}
 
 	private SourceWriter getSourceWriter(TreeLogger logger, GeneratorContext ctx) {
 
 		PrintWriter printWriter = ctx.tryCreate(logger, packageName, className);
-		if ( printWriter == null ) {
+		if (printWriter == null) {
 			return null;
 		}
 
 		ClassSourceFileComposerFactory composerFactory = new ClassSourceFileComposerFactory(
 				packageName, className);
-		
-//		composerFactory.addImport(SerializationStreamWriter.class.getName());
-//		composerFactory.addImplementedInterface("Serializer");
-		
+
+		// composerFactory.addImport(SerializationStreamWriter.class.getName());
+		// composerFactory.addImplementedInterface("Serializer");
+
 		return composerFactory.createSourceWriter(ctx, printWriter);
 	}
 
