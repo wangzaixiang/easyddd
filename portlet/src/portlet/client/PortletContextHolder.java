3c9bfff8a9b84d73c766fac3ee748e7c18810f4f
whgcompt <whgcompt@b89ababa-7a29-0410-b27f-27df3c269e39>
2007-04-06 12:49:00 +0000

    GWT.create()的框架代码

--- portlet/src/portlet/client/PortletContextHolder.java
+++ portlet/src/portlet/client/PortletContextHolder.java
@@ -3,6 +3,7 @@ package portlet.client;
 import portlet.client.impl.DefaultPortletContext;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
 
 public class PortletContextHolder implements EntryPoint {
@@ -22,10 +23,17 @@ public class PortletContextHolder implements EntryPoint {
 	private static native PortletContext doImport(JavaScriptObject jso) /*-{
 		return @portlet.client.PortletContextHelper::doImport(Lcom/google/gwt/core/client/JavaScriptObject;)(jso);
 	}-*/;
-	
+
+	private static native void print(Object obj) /*-{
+		var s = obj.@portlet.client.PortletContext_Helper::getString()();
+		$wnd.alert(s);
+	}-*/;
+		
 	public void onModuleLoad() {
 		JavaScriptObject jso = get();
 		if ( null == jso ) {
+			Object obj = GWT.create(PortletContext.class);
+			print(obj);
 			PortletContext pc = new DefaultPortletContext();
 			jso = doExport(pc);
 			put(jso);
