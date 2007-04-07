48441fe66151a336c065255c5f20a450d6f20c6b
whgcompt <whgcompt@b89ababa-7a29-0410-b27f-27df3c269e39>
2007-04-07 14:04:43 +0000

    PortletInfo.create()返回Portlet，

--- portlet/demo/src/examples/example_1/client/Example_1.java
+++ portlet/demo/src/examples/example_1/client/Example_1.java
@@ -1,5 +1,6 @@
 package examples.example_1.client;
 
+import portlet.client.Portlet;
 import portlet.client.PortletContext;
 import portlet.client.PortletContextHolder;
 import portlet.client.PortletInfo;
@@ -12,33 +13,34 @@ public class Example_1 {
 	public void onModuleLoad() {
 		checkModules();
 		PortletInfo pollInfo = findPortletInfoByName("poll");
-		if ( null == pollInfo ) {
+		if (null == pollInfo) {
 			;
 		} else {
-			pollInfo.create(DOM.getElementById("_poll_container_1"), "001");
-			pollInfo.create(DOM.getElementById("_poll_container_2"), "002");
+			Portlet poll = pollInfo.create("001");
+			poll.render(DOM.getElementById("_poll_container_1"));
+			poll = pollInfo.create("002");
+			poll.render(DOM.getElementById("_poll_container_2"));
 		}
 	}
-	
+
 	private PortletInfo findPortletInfoByName(String name) {
 		PortletContext pc = PortletContextHolder.getPortletContext();
 		int count = pc.getLength();
-		for ( int i = 0; i < count; i++ ) {
-			if ( pc.getPortletInfo(i).getName().equals(name) ) {
+		for (int i = 0; i < count; i++) {
+			if (pc.getPortletInfo(i).getName().equals(name)) {
 				return pc.getPortletInfo(i);
 			}
 		}
 		return null;
 	}
-	
+
 	private void checkModules() {
 		PortletContext pc = PortletContextHolder.getPortletContext();
 		int count = pc.getLength();
 		Window.alert("count of modules: " + count);
-		for ( int i = 0; i < count; i++ ) {
+		for (int i = 0; i < count; i++) {
 			String name = pc.getPortletInfo(i).getName();
 			Window.alert("registered portlet: " + name);
 		}
 	}
 }
-
--- portlet/src/portlet/client/Portlet.java
+++ portlet/src/portlet/client/Portlet.java
@@ -2,7 +2,7 @@ package portlet.client;
 
 import com.google.gwt.user.client.Element;
 
-public interface Portlet {
+public interface Portlet extends Portable {
 
 	void render(Element container);
 	
